3a0c347b761e58cb2eafcfc622cc6a19d9c49c58
Orlin Nedyalkov <onedyalkov@propeople.dk>
2015-09-29 11:13:31 +0300

    IHTML-59 'drupal' branch - added page.tpl (with some corrections) and remove unwanted CSS

--- template.php
+++ template.php
@@ -23,3 +23,45 @@ function ffw_preprocess_html(&$vars) {
     'preprocess' => FALSE
   ));
 }
+
+/**
+ * Alter CSS files before they are output on the page.
+ */
+function ffw_css_alter(&$css) {
+  // Remove Drupal core CSS.
+  $exclude = array(
+    'modules/aggregator/aggregator.css' => FALSE,
+    'modules/block/block.css' => FALSE,
+    'modules/book/book.css' => FALSE,
+    'modules/comment/comment.css' => FALSE,
+    'modules/dblog/dblog.css' => FALSE,
+    'modules/field/theme/field.css' => FALSE,
+    'modules/file/file.css' => FALSE,
+    'modules/filter/filter.css' => FALSE,
+    'modules/forum/forum.css' => FALSE,
+    'modules/help/help.css' => FALSE,
+    'modules/menu/menu.css' => FALSE,
+    'modules/node/node.css' => FALSE,
+    'modules/openid/openid.css' => FALSE,
+    'modules/poll/poll.css' => FALSE,
+    'modules/profile/profile.css' => FALSE,
+    'modules/search/search.css' => FALSE,
+    'modules/statistics/statistics.css' => FALSE,
+    'modules/syslog/syslog.css' => FALSE,
+    'modules/system/admin.css' => FALSE,
+    'modules/system/maintenance.css' => FALSE,
+    'modules/system/system.css' => FALSE,
+    'modules/system/system.admin.css' => FALSE,
+    'modules/system/system.maintenance.css' => FALSE,
+    'modules/system/system.messages.css' => FALSE,
+    'modules/system/system.menus.css' => FALSE,
+    'modules/system/system.theme.css' => FALSE,
+    'modules/taxonomy/taxonomy.css' => FALSE,
+    'modules/tracker/tracker.css' => FALSE,
+    'modules/update/update.css' => FALSE,
+    'modules/user/user.css' => FALSE,
+    'misc/vertical-tabs.css' => FALSE,
+    drupal_get_path('module', 'views') . '/css/views.css' => FALSE,
+  );
+  $css = array_diff_key($css, $exclude);
+}
--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -45,17 +45,23 @@
 ?><!doctype html>
 <html lang="<?php print $language->language; ?>">
 <head>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
 </head>
+
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
+
+<div id="skip-link">
+  <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+</div>
+
+<?php print $page_top; ?>
+
+<?php print $page; ?>
+
+<?php print $page_bottom; ?>
+
 </body>
 </html>
