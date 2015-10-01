8baef7fc687ec9a2da5c3bc936516f2a2e27b719
Orlin Nedyalkov <onedyalkov@propeople.dk>
2015-10-01 11:21:49 +0300

    IHTML-59 drupal7 branch - codesniffer corrections

--- template.php
+++ template.php
@@ -14,13 +14,13 @@ function ffw_preprocess_html(&$vars) {
   // Add our custom CSS after all other CSS.
   drupal_add_css(path_to_theme() . '/compiled/styles.css', array(
     'weight' => 999,
-    'preprocess' => FALSE
+    'preprocess' => FALSE,
   ));
   // Add our custom JS in footer, after all other JS.
   drupal_add_js(path_to_theme() . '/compiled/script.js', array(
     'scope' => 'footer',
     'weight' => 999,
-    'preprocess' => FALSE
+    'preprocess' => FALSE,
   ));
 }
 
