7371294521d9112cb3ac2c4d4b20385cbfc64a58
Svetlin Marinov <svetlin.marinov@ffwagency.com>
2018-11-12 10:54:13 +0200

    FFW theme v.2, initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-!function(a,b){console.log("FFW FE theme"),b.true=a}({},function(){return this}());
+/**
+ * @file
+ * FFW FE theme custom JS.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.feThemeExample = {
+    attach: function (context, settings) {
+      console.log('FFW FE theme');
+    }
+  };
+
+}(jQuery, Drupal));
---
+++ assets/sass/content/README.md
@@ -0,0 +1,7 @@
+Styles specific for the content type (e.g. News).
+
+.page-node-type-news {
+  main {
+    padding: $gutter 0 $gutter * 2;
+  }
+}
